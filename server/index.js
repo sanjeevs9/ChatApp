293313778b72b5eed17918292f0554f763d675e6
sanjeevs9 <sanjeev.19kr@gmail.com>
2024-07-25 19:38:08 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -1,13 +1,51 @@
 const express =require("express");
 const cors=require("cors");
-
+const {createServer} =require("http")
 const app=express();
+const { Server } = require("socket.io");
+const { log } = require("console");
+const { SocketAddress } = require("net");
+
 
 app.use(cors());
 app.use(express.json());
-const PORT=3000;
+const PORT=5000;
 
-app.listen(PORT,()=>{
+const httpServer=createServer(app);
+
+const io=new Server(httpServer,{
+    cors:{
+        origin:"http://localhost:5173"
+    }
+});
+
+io.on("connection",(socket)=>{
+    console.log("connected to web socket");
+    console.log(socket.id);
+
+    socket.emit("welcome","welcome to the server")
+    socket.broadcast.emit("welcome",`user has joind the server ${socket.id}`)
+
+    socket.on("message",({message,id})=>{
+            // socket.broadcast.emit("sent",data)
+            console.log(message);
+            console.log(id);
+            io.to(id).emit("sent",message);
+    })
+    
+
+    socket.on("join",(data)=>{
+        socket.join(data);
+    })
+
+    socket.on("disconnect",()=>{
+        console.log(`disconnect as ${socket.id}`)
+    })
+})
+
+
+
+httpServer.listen(PORT,()=>{
     console.log("backend connected on PORT :" +PORT);
 })
 
